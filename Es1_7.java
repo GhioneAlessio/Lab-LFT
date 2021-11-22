e3cebc180c2a63bb148867ddc8ac8baa07b58f64
Banananya <aleghio14@gmail.com>
2021-11-22 10:47:24 +0100

    Progetto

--- Es1_7.java
+++ Es1_7.java
@@ -1,33 +1,124 @@
 import java.util.Scanner;
 
 public class Es1_7 {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Inserisci il nome: ");
-        String nome = scanner.nextLine();
-        System.out.println("Inserisci la stringa da controllare: ");
-        String sr = scanner.nextLine();
-        scanner.close();
+    public static boolean scan(String s) {
         int state = 0;
-        for(int i = 0; state != -1 && i < sr.length(); i++){
-            switch(state){ 
-                case 0:
-                    if(sr.charAt(i) != nome.charAt(i))
-                        state = 1; 
+        for (int i = 0; state != -1 && i < s.length(); i++) {
+            char ch = s.charAt(i);
+            switch (state) {
+            case 0:
+                if (ch == 'A') {
+                    state = 1;
                     break;
-                case 1: 
-                    if(sr.charAt(i) == nome.charAt(i))
-                        state = 2;
-                    else 
-                        state = -1;
+                } else {
+                    state = 8;
                     break;
-                case 2: 
-                    if(sr.charAt(i) != nome.charAt(i))
-                        state = -1;
-            }
+                }
+            case 1:
+                if (ch == 'l') {
+                    state = 2;
+                    break;
+                } else {
+                    state = 9;
+                    break;
+                }
+            case 2:
+                if (ch == 'e') {
+                    state = 3;
+                    break;
+                } else {
+                    state = 10;
+                    break;
+                }
+            case 3:
+                if (ch == 's') {
+                    state = 4;
+                    break;
+                } else {
+                    state = 11;
+                    break;
+                }
+            case 4:
+                if (ch == 's') {
+                    state = 5;
+                    break;
+                } else {
+                    state = 12;
+                    break;
+                }
+            case 5:
+                if (ch == 'i') {
+                    state = 6;
+                    break;
+                } else {
+                    state = 13;
+                    break;
+                }
+            case 6:
+                state = 7;
+                break;
+            case 7:
+                state = -1;
+                break;
+            case 8:
+                if (ch == 'l') {
+                    state = 9;
+                    break;
+                } else {
+                    state = -1;
+                    break;
+                }
+            case 9:
+                if (ch == 'e') {
+                    state = 10;
+                    break;
+                } else {
+                    state = -1;
+                    break;
+                }
+            case 10:
+                if (ch == 's') {
+                    state = 11;
+                    break;
+                } else {
+                    state = -1;
+                    break;
+                }
+            case 11:
+                if (ch == 's') {
+                    state = 12;
+                    break;
+                } else {
+                    state = -1;
+                    break;
+                }
+            case 12:
+                if (ch == 'i') {
+                    state = 13;
+                    break;
+                } else {
+                    state = -1;
+                    break;
+                }
+            case 13:
+                if (ch == 'o') {
+                    state = 7;
+                    break;
+                } else {
+                    state = -1;
+                    break;
+                }
+            }  
         }
+        return state == 7;
+    }
 
-        System.out.printf("Stringa accetata: %s", (state == 0 || state == 2)? "Si" : "No");
-        
+    public static void main(String[] args) {
+        String st;
+        Scanner scanner = new Scanner(System.in);
+        st = scanner.nextLine();
+        scanner.close();
+        System.out.println(scan(st) ? "OK" : "NOPE");
+
     }
 }
