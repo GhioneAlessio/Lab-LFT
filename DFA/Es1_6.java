29930361362c43a40052a29f6a7b3b78df26c803
Banananya <aleghio14@gmail.com>
2022-01-21 16:26:11 +0100

    Final Fix si spera

--- DFA/Es1_6.java
+++ DFA/Es1_6.java
@@ -2,133 +2,136 @@ import java.util.Scanner;
 
 public class Es1_6 {
     public static boolean scan(String s) {
-        int state  = 0;
+        int state = 0;
         int i = 0;
         while (state != -1 && i < s.length()) {
             char ch = s.charAt(i);
-            if(Character.isLetter(ch))
+            if (Character.isLetter(ch))
                 ch = Character.toLowerCase(ch);
             switch (state) {
-            case 0 :
-                if (Character.isDigit(ch)){
-                    if(ch % 2 == 0){
-                        state = 1;
-                        break;
-                    }
-                    else{
-                        state = 2;
-                        break;
-                    }
-                }
-                else 
-                    state = -1;
-                break;
-            case 1:
-                if (Character.isDigit(ch)){
-                    if(ch % 2 == 0){
-                        state = 3;
-                        break;
-                    }
-                    else{
-                        state = 4;
-                        break;
-                    }
-                }
-                else
-                    state = -1;
-                break;
-            case 2:
-                if (Character.isDigit(ch)){
-                    if(ch % 2 == 0){
-                        state = 5;
-                        break;
-                    }
-                    else{
-                        state = 6;
-                        break;
-                    }
-                }
-                else
-                    state = -1;
-                break;
-
-            case 3:
-                if(Character.isDigit(ch)){
-                    if(ch % 2 != 0){
-                        state = 4;
-                        break;
-                    }else 
-                    break;
-                }else if(Character.isLetter(ch)){
-                    if(97 <= ch && ch <= 107){
-                        state = 7;
-                    }else 
-                    state =-1;
-                    break;
-                }
-            case 4:
-                if(Character.isDigit(ch)){
-                    if(ch % 2 != 0){
-                        state = 6;
-                        break;
-                    }else{
-                        state = 5;
-                        break;
-                    }
-                    
-                }else if(Character.isLetter(ch)){
-                    if(97 <= ch && ch <= 107){
-                        state = 7;
-                    }else {
-                    state =-1;
-                    break;}
-                }else 
-                state = -1;
-                break;
-                
-            case 5:
-                if(Character.isDigit(ch)){
-                    if(ch % 2 != 0){
-                        state = 4;
-                        break;
-                    }else{
-                        state = 3;
-                        break;
-                    }
-                    
-                }else if(Character.isLetter(ch)){
-                    if(97 <= ch && ch <= 107){
+                case 0:
+                    if (Character.isDigit(ch)) {
+                        if (ch % 2 == 0) {
+                            state = 1;
+                            break;
+                        } else {
+                            state = 2;
+                            break;
+                        }
+                    } else
                         state = -1;
-                    }else {
-                    state =8;
-                    break;}
-                }else 
-                state = -1;
-                break;
+                    break;
+                case 1:
+                    if (Character.isDigit(ch)) {
+                        if (ch % 2 == 0) {
+                            state = 3;
+                            break;
+                        } else {
+                            state = 4;
+                            break;
+                        }
+                    } else
+                        state = -1;
+                    break;
+                case 2:
+                    if (Character.isDigit(ch)) {
+                        if (ch % 2 == 0) {
+                            state = 5;
+                            break;
+                        } else {
+                            state = 6;
+                            break;
+                        }
+                    } else
+                        state = -1;
+                    break;
 
-            case 6: 
-                if(Character.isDigit(ch)){
-                    if(ch % 2 != 0){
-                        state = 6;
-                        break;
-                    }else{
-                        state = 5;
+                case 3:
+                    if (Character.isDigit(ch)) {
+                        if (ch % 2 != 0) {
+                            state = 4;
+                            break;
+                        } else
+                            break;
+                    } else if (Character.isLetter(ch)) {
+                        if (97 <= ch && ch <= 107) {
+                            state = 7;
+                        } else
+                            state = -1;
                         break;
                     }
-                    
-                }else if(Character.isLetter(ch)){
-                    if(97 <= ch && ch <= 107){
+                case 4:
+                    if (Character.isDigit(ch)) {
+                        if (ch % 2 != 0) {
+                            state = 6;
+                            break;
+                        } else {
+                            state = 5;
+                            break;
+                        }
+
+                    } else if (Character.isLetter(ch)) {
+                        if (97 <= ch && ch <= 107) {
+                            state = 7;
+                        } else {
+                            state = -1;
+                            break;
+                        }
+                    } else
                         state = -1;
-                    }else {
-                    state =8;
-                    break;}
-                }else 
-                state = -1;
-                break;
+                    break;
+
+                case 5:
+                    if (Character.isDigit(ch)) {
+                        if (ch % 2 != 0) {
+                            state = 4;
+                            break;
+                        } else {
+                            state = 3;
+                            break;
+                        }
+
+                    } else if (Character.isLetter(ch)) {
+                        if (97 <= ch && ch <= 107) {
+                            state = -1;
+                        } else {
+                            state = 7;
+                            break;
+                        }
+                    } else
+                        state = -1;
+                    break;
+
+                case 6:
+                    if (Character.isDigit(ch)) {
+                        if (ch % 2 != 0) {
+                            state = 6;
+                            break;
+                        } else {
+                            state = 5;
+                            break;
+                        }
+                    } else if (Character.isLetter(ch)) {
+                        if (97 <= ch && ch <= 107) {
+                            state = -1;
+                        } else {
+                            state = 7;
+                            break;
+                        }
+                    } else
+                        state = -1;
+                    break;
+                case 7:
+                    if (Character.isLetter(ch))
+                        break;
+                    else {
+                        state = -1;
+                        break;
+                    }
             }
-            i+=1;
-    }
-    return state == 7 || state == 8;
+            i += 1;
+        }
+        return state == 7;
     }
 
     public static void main(String[] args) {
